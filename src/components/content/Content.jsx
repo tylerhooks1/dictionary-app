e8731113dc89b873124f29f38459b384ed3ac1bd
Hooks <h377154@ml-jcf3p2l942.attlocal.net>
2023-06-10 12:56:43 -0500

    updates

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,39 +1,58 @@
 import React from "react";
-import { Container } from "@chakra-ui/react";
 import play from "../../assets/icon-play.svg";
 import "./content.css";
 
 const Content = () => {
   return (
-    <Container>
-      <div className="dictionary__content">
-        <div className="dictionary__content__header">
-          <div className="dictionary__content__header-title">
-            <h1>Word</h1>
-            <p>Phonetic</p>
-          </div>
-          <button className="dictionary__content__audio">Audio</button>
+    <div className="dictionary__content">
+      <div className="dictionary__content__header">
+        <div className="dictionary__content__header-title">
+          <h1>Word</h1>
+          <p>Phonetic</p>
         </div>
-        <div className="dictionary__content-noun">
-          <div className="dictionary__content-heading-noun">
-            <p>Noun</p>
-            <span />
-          </div>
-          <p>Meaning</p>
-          <ol className="dictionary__content__list-noun">
-            <li>something</li>
-          </ol>
-          {/**
-           * api.map(()=>{
-           *  <ol>
-           * <li>something</li>
-           * </ol>
-           * })
-           */}
-          <p>Synomyms</p>
-        </div>
+        <button className="dictionary__content__audio">
+          <img src={play} alt="play" height={48} width={48} />
+        </button>
       </div>
-    </Container>
+      <div className="dictionary__content-noun">
+        <div className="dictionary__content-heading">
+          <p>Noun</p>
+          <span />
+        </div>
+        <p>Meaning</p>
+        <ol className="dictionary__content__list-noun">
+          <li>something</li>
+        </ol>
+        {/**
+         * api.map(()=>{
+         *  <ol>
+         * <li>something</li>
+         * </ol>
+         * })
+         */}
+        <p>Synomyms</p>
+      </div>
+      <div className="dictionary__content-verb">
+        <div className="dictionary__content-heading">
+          <p>Verb</p>
+          <span />
+        </div>
+        <p>Meaning</p>
+        <ol className="dictionary__content__list-noun">
+          <li>something</li>
+        </ol>
+        {/**
+         * api.map(()=>{
+         *  <ol>
+         * <li>something</li>
+         * </ol>
+         * })
+         */}
+      </div>
+      <div className="dictionary__content-source">
+        <p>Source</p>
+      </div>
+    </div>
   );
 };
 
